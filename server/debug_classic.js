1c1287ebe25f1049a49b7c0daa981ec47f49d940
DonnchaFinlay <donncha.finlay@gmail.com>
2026-01-17 12:04:46 +0100

    Clean up

--- server/debug_classic.js
+++
@@ -1,7 +0,0 @@
-try {
-    const mod = require('@langchain/classic/vectorstores/memory');
-    console.log('SUCCESS: Loaded @langchain/classic/vectorstores/memory');
-    console.log('Exports:', Object.keys(mod));
-} catch (e) {
-    console.log('FAILURE:', e.message);
-}
